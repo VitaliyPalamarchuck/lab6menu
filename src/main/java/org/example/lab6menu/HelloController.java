105d9136d5285c40d57b00dd134c391b06711224
“VitaliyPalamarchuk“ <vitaliyypalamarchuck@gmail.com>
2025-12-09 15:50:52 +0200

    lab6menuPart

--- src/main/java/org/example/lab6menu/HelloController.java
+++ src/main/java/org/example/lab6menu/HelloController.java
@@ -3,10 +3,22 @@ package org.example.lab6menu;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class HelloController {
+    @FXML
+    private RadioMenuItem first;
+    @FXML
+    private TextField textField;
+    @FXML
+    private RadioMenuItem second;
+    @FXML
+    private ToggleGroup toggleContextText;
+
     private Stage newStage;
     @FXML
     private AnchorPane scenePane;
@@ -19,5 +31,11 @@ public class HelloController {
         System.out.println("Success");
         newStage.close();
     }
-
+    @FXML
+    void toggleContextText(ActionEvent event) {
+        if (toggleContextText.getSelectedToggle().equals(this.first))
+            textField.setText("RandomText");
+        else if (toggleContextText.getSelectedToggle().equals(this.second))
+            textField.setText(null);
+    }
 }
